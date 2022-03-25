fafa447fd312f4797e2cded8914928557af4a7bd
Ava <99355031+AvaVenturino@users.noreply.github.com>
2022-03-25 17:05:00 +0000

    added code

---
+++ js/script.js
@@ -0,0 +1,22 @@
+// Copyright (c) 2022 Ava Venturino All rights reserved
+//
+// Created by: Ava Venturino
+// Created on: Mar 2022
+// This file contains the JS functions for index.html
+
+'use strict'
+/**
+ * This function calculates the area of a triangle.
+ */
+function calculate () {
+  // input
+  const hoursWorked = parseInt(document.getElementById("number--of-hours-worked").value)
+  const hourlyRate = parseInt(document.getElementById("hourly-rate").value)
+  // process
+  const pay = (hoursWorked * hourlyRate) * (1.00 - 0.18)
+  const taxes = (hoursWorked * hourlyRate) * 0.18
+  // output
+  document.getElementById("employment-info").innerHTML =
+    "Your pay will be: " + ' $' + pay.toFixed(2)
+    "The government will take: " + ' $' + taxes.toFixed(2)
+}
